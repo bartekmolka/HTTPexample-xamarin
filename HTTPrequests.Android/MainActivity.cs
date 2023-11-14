3459269fede58c31907b5c87ee2949e8cb59cd50
BartoszTurczynski <bart.turczynski@gmail.com>
2023-11-14 20:02:58 +0100

    arbeit macht frei

--- HTTPrequests.Android/MainActivity.cs
+++ HTTPrequests.Android/MainActivity.cs
@@ -5,8 +5,10 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 
+
 namespace HTTPrequests.Droid
 {
+
     [Activity(Label = "HTTPrequests", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
--- HTTPrequests/MainPage.xaml.cs
+++ HTTPrequests/MainPage.xaml.cs
@@ -9,6 +9,8 @@ using Xamarin.Forms;
 using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Diagnostics;
 
 namespace HTTPrequests
 {
@@ -35,12 +37,39 @@ namespace HTTPrequests
     {
         public Root todoObject;
 
+
+        public ICommand DeleteCommand { get; set; }
         public ObservableCollection<Todo> TodosCollection { get; set; }
+
+
+        private async Task BombAnimationAsync()
+        {
+            // Create a scale animation on a UI element (e.g., an image)
+            Image bombImage = new Image
+            {
+                Source = "bomb_icon.png", // Replace with your image source
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Content = bombImage;
+
+            // Scale animation
+            await bombImage.ScaleTo(1.2, 300, Easing.CubicInOut);
+            await Task.Delay(100);
+            await bombImage.ScaleTo(1, 200, Easing.CubicInOut);
+            await Task.Delay(100);
+            await bombImage.RotateTo(360, 500, Easing.SpringOut);
+            await Task.Delay(100);
+            await bombImage.RotateTo(0, 500, Easing.SpringIn);
+        }
+
 
         public MainPage()
         {
-            InitializeComponent();
             TodosCollection = new ObservableCollection<Todo>();
+            
+            InitializeComponent();
 
             // Call the request method and wait for the result
             Task.Run(async () =>
@@ -48,11 +77,13 @@ namespace HTTPrequests
                 var o = await Request();
                 todoObject = o; // Assign the result to your todoObject field
 
+                if (TodosCollection.Count > 0 ? true : false) return;
                 if (todoObject?.todos != null)
                 {
                     foreach (var todo in todoObject.todos)
                     {
                         TodosCollection.Add(todo);
+                        Console.WriteLine(todo);
                     }
                 }
 
@@ -60,8 +91,32 @@ namespace HTTPrequests
                 OnPropertyChanged(nameof(TodosCollection)); // Notify that the TodosCollection property has changed
             });
 
+             DeleteCommand = new Command<Todo>(item => 
+            {
+                TodosCollection.Clear();
+                BombAnimationAsync();
+                Console.WriteLine("This is a debug message.");
+                List<Todo> copyList = new List<Todo>();
+                int index = TodosCollection.IndexOf(item);
+                for (int i = 0; i < copyList.Count; i++)
+                {
+                    if (i == 0) // 0-based index, so the fifth element is at index 4
+                    {
+                        // Delete the fifth element
+                        TodosCollection.Clear();
+                        continue; // Exit the loop after deleting the fifth element
+                    }
+                    copyList.Add(TodosCollection[i]);
+                }
+                ObservableCollection<Todo> cp = new ObservableCollection<Todo>(copyList);
+
+                // Assign the modified copy back to the original ObservableCollection
+                TodosCollection = cp;
+            });
+
             BindingContext = this; // Set the BindingContext to the MainPage instance
         }
+        
 
         async Task<Root> Request()
         {
